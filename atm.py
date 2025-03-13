dd18c755b701990eb863423d4e8df0a1d95ac1fb
hoon <eoeo4652@naver.com>
2025-03-13 18:16:43 +0900

    ✨ 영수증 기능 추가
    영수증 기능 추가, 영수증 기능 보완

--- atm.py
+++ atm.py
@@ -2,6 +2,11 @@
 # 1. 입금, 2. 출금, 3. 영수증 보기, 4. 종료 => 글자를 입력받을지 / 숫자를 입력받을지
 # 숫자로 원하는 기능을 입력할 수 있게 만들어주섿요. 그리고 사용자가 입력한 기능을 num에 넣어주세요.
 # deposit_amount: 
+# withdraw_amount:
+# 영수증 기능에는 사용되는 데이터타입은 list => [["입금", 5000, 8000], ["출금", 3000, 5000], ["출금", 2000, 3000]
+
+# 원금을 담는 변수
+
 """
 balance = 3000
 
@@ -28,6 +33,7 @@ while True:
 
 """
 
+receipts = []
 balance = 3000
 
 while True:
@@ -38,6 +44,9 @@ while True:
         if deposit_amount.isdigit() and int(deposit_amount) > 0:  #첫번째 조건 문자가 입력된건 아닌지 확인 / 0 보다 큰 금액을 입력했는지 
             balance += int(deposit_amount) # balance = balance + int(deposit_amount)
             print(f"고객님이 입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+        
+            # 영수증에 입금 데이터 넣기
+            receipts.append(("입금", deposit_amount, balance))
         else:
             print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘!!!")
     if num == "2":
@@ -46,10 +55,20 @@ while True:
             withdraw_amount = min(balance, int(withdraw_amount))
             balance -= withdraw_amount # balance = balance - withdraw_amount
             print(f"고객님이 출금한 금액은 {withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+        
+        # 영수증에 출금 데이터 넣기
+            receipts.append(("출금", withdraw_amount, balance))
         else:
             print("정신차리고, 제대로된 숫자형태로 입금액을 작성해줘!!!")
     if num == "3":
-        pass
+        # 리스트에 값이 있을때와 없을때 달랐으면 합니다.
+        # 값이 있을때 출력 결과가 출금 : 3000원 | 잔액 : 5000원과 같이 나왔으면 합니다.
+        if receipts:
+            print("---영수증---")
+            for i in receipts:
+                print(f"{i[0]} : {i[1]}원 | 잔액 {i[2]}원")
+            print("캬~ 영수증에 아무내역도 없네요??")
+        
     if num == "4":
         print("서비스를 종료합니다.")
         break
---
+++ test1.py
@@ -0,0 +1,14 @@
+"""
+lis = []
+
+if lis:
+    print("True")
+else:
+    print("False")
+"""
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for i in a:
+    print(i)      # --> [1, 2, 3] [4, 5, 6] [7, 8, 9]
+
+    print(i[1])   # --> 2 5 8
